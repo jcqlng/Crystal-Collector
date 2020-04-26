23a1c76900825d2bcbdf6873d069f558441eee1c
jcqlng <jcqlngeiger@outlook.com>
2020-04-25 20:53:55 -0400

    added variables started math function for numbers to generate

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// Variables 
+$(document).ready(function(){
+    var wins = 0;
+    var lost = 0;
+    var score = 0;
+// variable/ math to do a random number generator
+    var computerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
+    
+})
