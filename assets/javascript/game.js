7c24be357a8cc4b3d4ad059e5a711439d77d5bdb
jcqlng <jcqlngeiger@outlook.com>
2020-04-27 08:19:48 -0400

    restart java script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,30 @@
-// Variables 
-var yourScore = document.getElementById('yourScore');
+// Global Variables 
 
-// Displays the current score
-yourScore.innerHTML = 0; 
-var targetScore = document.getElementById('targetScore');
-targetScore.innerHTML = getRandomValue(10, 4);
+// Crystal variables
+var Crystal= {
+  blue
+  {
+    name: "Blue",
+    value: 0
+  },
 
-function addToScore(val) {
-  var numberToAdd = parseInt(val);
-  var currentScore = parseInt(yourScore.innerHTML);
-  yourScore.innerHTML = numberToAdd + currentScore;
-} 
+  green
+  {
+    name: "Green",
+    value: 0,
+  },
 
-var red = document.getElementById("red");
-console.log(red.value);
+  red
+  {
+    name: "Red",
+    value:0,
+  },
 
-var blue = document.getElementById("blue");
+  yellow
+  {
+    name: "Yellow",
+    value:0,
+  }
 
-var yellow = document.getElementById("green");
+};
 
-var green = document.getElementById("yellow");
-
-red.addEventListener('click', function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-blue.addEventListener('click', function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-yellow.addEventListener('click', function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-green.addEventListener('click', function() {
-  addToScore(this.value);
-  checkPlayerScore();
-});
-
-function getRandomValue (a, b){
-  return Math.floor((Math.random() * a) + b);
-}
-
-function checkPlayerScore() {
-    counter += gemValue;
-
-    console.log(gemValue);
-    console.log(counter);
-  var playerInt = parseInt(yourScore.innerHTML);
-  var targetInt = parseInt(targetScore.innerHTML);
-  if (playerInt === targetInt) {
-      alert('You win!');
-  } else if (playerInt > targetInt) {
-    alert('You lose!');
-  } 
-}
-
-console.log(getRandomValue(10, 4));
