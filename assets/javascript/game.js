73067ed7767d9c4403d0c86028c64bfa9d38c4ad
jcqlng <jcqlngeiger@outlook.com>
2020-04-27 05:04:24 -0400

    added click functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,25 +13,48 @@ function addToScore(val) {
 } 
 
 // the values of each gem and the console log -- retrieved by website in readme
-var red = document.getElementById("crystal-1");
+var red = document.getElementById("red");
 //red.setAttribute("value", "5");
 console.log(red.value);
 
-var blue = document.getElementById("crystal-2");
-//console.log(blue.value);
+var blue = document.getElementById("blue");
 
-var yellow = document.getElementById("crystal-3");
-//console.log(yellow.value);
+var yellow = document.getElementById("green");
 
-var green = document.getElementById("crystal-4");
-//console.log(green.value);
+var green = document.getElementById("yellow");
 
-// add an event listener to elements
+// event listener to elements
 red.addEventListener('click', function() {
   //this refers to red
   addToScore(this.value);
   checkPlayerScore();
 });
+
+blue.addEventListener('click', function() {
+  // this refers to blue
+  addToScore(this.value);
+  checkPlayerScore();
+});
+
+yellow.addEventListener('click', function() {
+  // this refers to blue
+  addToScore(this.value);
+  checkPlayerScore();
+});
+
+green.addEventListener('click', function() {
+  // this refers to blue
+  addToScore(this.value);
+  checkPlayerScore();
+});
+//var sum = parseInt(red.value) + parseInt(blue.value) + parseInt(yelow.value) + parseInt(green.value);
+
+// console.log (sum);
+function getRandomValue (a, b){
+  return Math.floor((Math.random() * a) + b);
+}
+
+function checkPlayerScore() {
     counter += gemValue;
 
     console.log(gemValue);
